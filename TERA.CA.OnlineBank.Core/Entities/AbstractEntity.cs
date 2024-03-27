b6fc138a2e6be661e7a0b47dfe1abd53f250b7fb
Guga Apkhazava <Guga Apkhazava@DESKTOP-EEG6HFA>
2024-03-27 17:56:56 +0400

    add: Core layer add User entity

---
+++ TERA.CA.OnlineBank.Core/Entities/AbstractEntity.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TERA.CA.OnlineBank.Core.Entities
+{
+    public abstract class AbstractEntity
+    {
+        [Key]
+        protected Guid Id { get; set; }
+    }
+}
---
+++ TERA.CA.OnlineBank.Core/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TERA.CA.OnlineBank.Core.Entities
+{
+    [Table("Users")]
+    [Index(nameof(PersonalNumber),IsUnique =true)]
+    public class User:IdentityUser
+    {
+        [Required]
+        [Column("Name_Of_User")]
+        public string Name { get; set; }
+
+        [Required]
+        [Column("Surname_Of_User")]
+        public string Surname { get; set; }
+
+        [Required]
+        [Column("Personal_Number")]
+        public string PersonalNumber { get; set; }
+    }
+}
