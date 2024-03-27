cba7862d2ab56e18fc9e253d9de4296c0e33978f
Guga Apkhazava <Guga Apkhazava@DESKTOP-EEG6HFA>
2024-03-27 21:50:22 +0400

    config: Add config in program.cs

--- TERA.CA.OnlineBank.UI/Program.cs
+++ TERA.CA.OnlineBank.UI/Program.cs
@@ -1,15 +1,65 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using TERA.CA.OnlineBank.Core.Data;
+using TERA.CA.OnlineBank.Core.Entities;
+using TERA.CA.OnlineBank.Core.Interfaces;
+using TERA.CA.OnlineBank.Core.Repositories;
+using TERA.CA.OnlineBank.UI.Hellpers;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDbContext<WalletDb>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("WalletDb"));
+});
+
+builder.Services.AddIdentity<User, IdentityRole>().
+AddEntityFrameworkStores<WalletDb>().
+AddDefaultTokenProviders();
+
+var validations = builder.Configuration.GetSection("TokenValidation").Get<TokenValidationSettings>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        if (validations != null)
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+
+                ValidateIssuer = validations.ValidateIssuer,
+                ValidateAudience = validations.ValidateAudience,
+                ValidateLifetime = validations.ValidateLifetime,
+                ValidateIssuerSigningKey = validations.ValidateIssuerSigningKey,
+                ValidIssuer = validations.ValidIssuer,
+                ValidAudience = validations.ValidAudience,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(validations.IssuerSigningKey?? "65E255FF-F399-42D4-9C7F-D5D08B0EC285")),
+            };
+        }
+    });
+
+builder.Services.AddScoped<RoleManager<IdentityRole>>();
+builder.Services.AddScoped<UserManager<User>>();
+builder.Services.AddScoped<SignInManager<User>>();
+
+builder.Services.AddScoped<IUniteOfWork, UniteOfWork>();
+builder.Services.AddScoped<ICurencyRepository, CurencyRepository>();
+builder.Services.AddScoped<ITransactionRepository, TransactionRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IWalletRepository, WalletRepository>();
 
 
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,9 +67,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseRouting();
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
