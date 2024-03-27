1ff16c0c6ae24a71cfc4563feff670ad4fdb3f6f
Guga Apkhazava <Guga Apkhazava@DESKTOP-EEG6HFA>
2024-03-27 18:39:45 +0400

    Add: Core layer , Add Context's

---
+++ TERA.CA.OnlineBank.Core/Data/MongoLoggerDb.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace TERA.CA.OnlineBank.Core.Data
+{
+    public class MongoLoggerDb//gamoviyeneb mongo db s logebs shesanaxad
+    {
+        private readonly MongoClient client;
+        public virtual IMongoCollection<BsonDocument> Logs { get; set; }
+
+        public MongoLoggerDb()
+        {
+            client = new MongoClient();
+            var database = client.GetDatabase("LogTerasData");
+            Logs = database.GetCollection<BsonDocument>("TerasLogs");
+        }
+    }
+}
---
+++ TERA.CA.OnlineBank.Core/Data/WalletDb.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TERA.CA.OnlineBank.Core.Entities;
+
+namespace TERA.CA.OnlineBank.Core.Data
+{
+    public class WalletDb:IdentityDbContext<User>
+    {
+        public WalletDb(DbContextOptions<WalletDb>ops):base(ops)
+        {
+                
+        }
+
+        public virtual DbSet<Curency> Curencies { get; set; }
+
+        public virtual DbSet<Transaction> Transactions { get; set; }
+
+        public virtual DbSet<Wallet> Wallets { get; set; }
+    }
+}
