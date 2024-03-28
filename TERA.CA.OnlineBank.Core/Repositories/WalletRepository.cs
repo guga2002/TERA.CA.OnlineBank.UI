bc3912b8b7509e7b83d548abf5b09fd69a05bfc3
Guga Apkhazava <Guga Apkhazava@DESKTOP-EEG6HFA>
2024-03-28 08:56:50 +0400

    Add: Repository to data layer , walletrepository

--- TERA.CA.OnlineBank.Core/Repositories/WalletRepository.cs
+++ TERA.CA.OnlineBank.Core/Repositories/WalletRepository.cs
@@ -1,4 +1,7 @@
-﻿using TERA.CA.OnlineBank.Core.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using SharpCompress.Common;
+using System.Security.AccessControl;
+using TERA.CA.OnlineBank.Core.Data;
 using TERA.CA.OnlineBank.Core.Entities;
 using TERA.CA.OnlineBank.Core.Interfaces;
 
@@ -6,43 +9,84 @@ namespace TERA.CA.OnlineBank.Core.Repositories
 {
     public class WalletRepository : AbstractRepository, IWalletRepository
     {
+        private readonly DbSet<Wallet> wallet;
         public WalletRepository(WalletDb db) : base(db)
         {
+            wallet = Context.Set<Wallet>();
         }
 
-        public Task<bool> Create(Wallet entity)
+        public async Task<bool> Create(Wallet entity)
         {
-            throw new NotImplementedException();
+            using (var transact = await Context.Database.BeginTransactionAsync())
+            {
+                if (!wallet.Any(io => io.UserId == entity.UserId && io.CurrencyId == entity.CurrencyId))
+                {
+                    await wallet.AddAsync(entity);
+                    await Context.SaveChangesAsync();
+                    await transact.CommitAsync();
+                    return true;
+                }
+                await transact.RollbackAsync();
+                return false;
+            }
         }
 
-        public Task<bool> Delete(Wallet entoty)
+        public async Task<bool> Delete(Wallet entoty)
         {
-            throw new NotImplementedException();
+            using (var transact = await Context.Database.BeginTransactionAsync())
+            {
+                var firs = wallet.Where(io => io.UserId == entoty.UserId && io.CurrencyId == entoty.CurrencyId).FirstOrDefault();
+                if (firs != null)
+                {
+                    wallet.Remove(firs);
+                    await Context.SaveChangesAsync();
+                    await transact.CommitAsync();
+                    return true;
+                }
+                await transact.RollbackAsync();
+                return false;
+            }
         }
 
-        public Task<IEnumerable<Wallet>> GetAll()
+        public async Task<IEnumerable<Wallet>> GetAll()
         {
-            throw new NotImplementedException();
+            return await wallet.ToListAsync();
         }
 
-        public Task<IEnumerable<Wallet>> GetAllWithDetails()
+        public async Task<IEnumerable<Wallet>> GetAllWithDetails()
         {
-            throw new NotImplementedException();
+            return await wallet.Include(io => io.Transactions).Include(io => io.Currency)
+                .ToListAsync();
         }
 
-        public Task<Wallet> GetById(Guid Id)
+        public async Task<Wallet> GetById(Guid Id)
         {
-            throw new NotImplementedException();
+            return await wallet.FindAsync(Id)??new Wallet();
         }
 
-        public Task<Wallet> GetByIdWithDetails(Guid Id)
+        public async Task<Wallet> GetByIdWithDetails(Guid Id)
         {
-            throw new NotImplementedException();
+            return await wallet.Include(io=>io.Transactions).Include(io=>io.Currency).FirstOrDefaultAsync(I=>I.Id==Id) ?? new Wallet();
         }
 
-        public Task<bool> Update(Wallet entity)
+        public async Task<bool> Update(Wallet entity)
         {
-            throw new NotImplementedException();
+            using (var transact = await Context.Database.BeginTransactionAsync())
+            {
+                var firs = await wallet.Where(io => io.UserId == entity.UserId).FirstOrDefaultAsync();
+                if (firs != null)
+                {
+                    firs.CurrencyId = entity.CurrencyId;
+                    firs.Amount = entity.Amount;
+
+                    wallet.Update(firs);
+                    await Context.SaveChangesAsync();
+                    await transact.CommitAsync();
+                    return true;
+                }
+                await transact.RollbackAsync();
+                return false;
+            }
         }
     }
 }
